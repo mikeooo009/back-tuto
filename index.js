7d34548a4afd0155d3f46acf2b5da679d9de2c7e
mikeooo009 <mike.java.code@gmail.com>
2024-12-20 18:13:57 +0100

    ds

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-const app = require('./server');
- 
-module.exports = app;
+const express = require('express');
+const app = express();
+app.use(express.json())
+const cors = require("cors");
+app.use(cors())
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+mongoose
+    .connect(process.env.DB, {
+
+    })
+const UserModel = require('./models/Users');
+
+app.get('/users', async (req, res) => {
+    const users = await UserModel.find();
+    res.json(users)
+})
+
+
+app.post('/createUser', async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user)
+})
+
+
+app.listen("3001", () => {
+
+    console.log("server works good!!");
+
+})
